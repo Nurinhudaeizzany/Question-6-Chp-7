f345a07d3a0a442a2f7489fa818e024dc9fdf177
Nurinhudaeizzany <56948639+Nurinhudaeizzany@users.noreply.github.com>
2019-11-14 12:50:55 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim weight As Double
+        Dim cost As String
+        weight = Val(TextBox1.Text)
+        Select Case weight
+            Case Is <= 2
+                cost = 2.5
+            Case 2.1 To 5
+                cost = 3.8
+            Case 5.1 To 10
+                cost = 6.0
+            Case 10.1 To 20
+                cost = 10
+            Case Is > 20
+                cost = "unacceptable"
+        End Select
+        TextBox2.Text = cost
+    End Sub
+End Class
